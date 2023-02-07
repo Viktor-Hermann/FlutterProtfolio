3e46162e3c7fb2e2d8894c5b5b45d396669125b1
Viktor Hermann <v.hermann.it@gmail.com>
2023-02-07 11:05:53 +0100

    Cleanup and rework home screen

--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:web_app/constants.dart';
-import 'package:web_app/sections/about/about_section.dart';
-import 'package:web_app/sections/contact/contact_section.dart';
-import 'package:web_app/sections/feedback/feedback_section.dart';
-import 'package:web_app/sections/recent_work/recent_work_section.dart';
-import 'package:web_app/sections/service/service_section.dart';
-import 'package:web_app/sections/topSection/top_section.dart';
+import 'package:web_app/utils/constants.dart';
+import 'package:web_app/presentation/about/about_section.dart';
+import 'package:web_app/presentation/contact/contact_section.dart';
+import 'package:web_app/presentation/feedback/feedback_section.dart';
+import 'package:web_app/presentation/recent_work/recent_work_section.dart';
+import 'package:web_app/presentation/service/service_section.dart';
+import 'package:web_app/presentation/topSection/top_section.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -22,10 +23,6 @@ class HomeScreen extends StatelessWidget {
             FeedbackSection(),
             SizedBox(height: kDefaultPadding),
             ContactSection(),
-            // This SizeBox just for demo
-            // SizedBox(
-            //   height: 500,
-            // )
           ],
         ),
       ),
