53633b74676ed02b6c536799f39be3ce34a6efb8
Viktor Hermann <v.hermann.it@gmail.com>
2023-02-07 11:09:37 +0100

    Add feedback section

---
+++ lib/presentation/feedback/feedback_section.dart
@@ -0,0 +1,42 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:web_app/infrastructure/datasource/feedback_data.dart';
+import 'package:web_app/presentation/widgets/section_title.dart';
+import 'package:web_app/utils/constants.dart';
+
+import 'widgets/feedback_card.dart';
+
+class FeedbackSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: kDefaultPadding * 2.5),
+      constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width),
+      child: Column(
+        children: [
+          SectionTitle(
+            title: 'feedbackTitle'.tr(),
+            subTitle: '',
+          ),
+          CarouselSlider.builder(
+            options: CarouselOptions(
+              height: 364.0,
+              enableInfiniteScroll: true,
+              autoPlay: true,
+              autoPlayInterval: Duration(seconds: 5),
+              animateToClosest: true,
+              autoPlayCurve: Curves.fastOutSlowIn,
+            ),
+            itemCount: feedbacks.length,
+            itemBuilder:
+                (BuildContext context, int itemIndex, int pageViewIndex) =>
+                    FeedbackCard(
+              index: itemIndex,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/presentation/feedback/widgets/feedback_card.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:web_app/infrastructure/datasource/feedback_data.dart';
+
+import '../../../utils/constants.dart';
+
+class FeedbackCard extends StatefulWidget {
+  const FeedbackCard({
+    Key key,
+    this.index,
+  }) : super(key: key);
+
+  final int index;
+
+  @override
+  _FeedbackCardState createState() => _FeedbackCardState();
+}
+
+class _FeedbackCardState extends State<FeedbackCard> {
+  Duration duration = Duration(milliseconds: 200);
+  bool isHover = false;
+  @override
+  Widget build(BuildContext context) {
+    final feedback = feedbacks[widget.index];
+
+    return AnimatedContainer(
+      duration: duration,
+      margin: EdgeInsets.only(top: kDefaultPadding * 3),
+      padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      width: MediaQuery.of(context).size.width * 0.5,
+      decoration: BoxDecoration(
+        color: feedback.color,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Transform.translate(
+            offset: Offset(0, -50),
+            child: AnimatedContainer(
+              duration: duration,
+              height: 124,
+              width: 124,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.white, width: 10),
+                image: DecorationImage(image: AssetImage(feedback.userPic)),
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              SizedBox(height: kDefaultPadding * 4),
+              Text(
+                feedback.name,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+              Text(
+                feedback.company,
+                style: TextStyle(
+                  color: kTextColor,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  height: 1.5,
+                ),
+              ),
+              Text(
+                feedback.role,
+                style: TextStyle(
+                  color: kTextColor,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w300,
+                  height: 1.5,
+                ),
+              ),
+              SizedBox(height: kDefaultPadding),
+              Text(
+                feedback.review,
+                style: TextStyle(
+                  color: kTextColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w300,
+                  fontStyle: FontStyle.italic,
+                  height: 1.5,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
