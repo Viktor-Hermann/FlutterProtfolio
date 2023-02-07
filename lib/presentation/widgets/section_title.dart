ecee061a47893b7923cb0bc0b6a67ed27fc91627
Viktor Hermann <v.hermann.it@gmail.com>
2023-02-07 11:06:16 +0100

    Cleanup and rework section title widget

---
+++ lib/presentation/widgets/section_title.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/constants.dart';
+
+class SectionTitle extends StatelessWidget {
+  const SectionTitle({
+    Key key,
+    this.title,
+    this.subTitle,
+    this.color,
+  }) : super(key: key);
+
+  final String title, subTitle;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: kDefaultPadding),
+      constraints: BoxConstraints(maxWidth: 1110),
+      height: 100,
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              subTitle,
+              style: TextStyle(
+                  fontWeight: FontWeight.w200, color: Color(0xFF304481)),
+            ),
+            Text(
+              title,
+              style: Theme.of(context).textTheme.displayMedium.copyWith(
+                  fontWeight: FontWeight.bold, color: Color(0xFF304481)),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
