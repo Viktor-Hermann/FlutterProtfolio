1859d5caf097ce391ea2ba6573e545ae8926c8c5
Viktor Hermann <v.hermann.it@gmail.com>
2023-02-07 11:07:39 +0100

    Cleanup and rework service widget

---
+++ lib/presentation/service/service_section.dart
@@ -0,0 +1,38 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:web_app/infrastructure/datasource/services_data.dart';
+import 'package:web_app/presentation/widgets/section_title.dart';
+import 'package:web_app/utils/constants.dart';
+
+import 'widgets/service_card.dart';
+
+class ServiceSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: kDefaultPadding * 2),
+      constraints: BoxConstraints(maxWidth: 1110),
+      child: Column(
+        children: [
+          SectionTitle(
+            color: Colors.grey[600],
+            title: 'serviceTitle'.tr(),
+            subTitle: 'serviceSubtitle'.tr(),
+          ),
+          GridView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: services.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+              childAspectRatio: 1.0,
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+            ),
+            itemBuilder: (context, index) => ServiceCard(index: index),
+          )
+        ],
+      ),
+    );
+  }
+}
