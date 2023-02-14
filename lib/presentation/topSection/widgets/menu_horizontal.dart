1eb08980c450833a7d3a2635c7e142de7b180059
Viktor Hermann <v.hermann.it@gmail.com>
2023-02-14 12:09:43 +0100

    Improve and rework menu

--- lib/presentation/topSection/widgets/menu_horizontal.dart
+++ lib/presentation/topSection/widgets/menu_horizontal.dart
@@ -1,26 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:web_app/generated/l10n.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:web_app/application/topsection/menu_controller.dart';
 
 import '../../../utils/constants.dart';
 
 class MenuHorizontal extends ConsumerWidget {
   const MenuHorizontal({Key key}) : super(key: key);
 
-class _MenuHorizontalState extends State<MenuHorizontal> {
-  int selectedIndex = 0;
-  int hoverIndex = 0;
-  List<String> menuItems = [
-    '',
-    S.current.menuHome,
-    S.current.menuAboutMe,
-    S.current.menuServices,
-    S.current.menuMyProjects,
-    S.current.menuFeedback,
-    S.current.menuContact,
-    ''
-  ];
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final menuItems = getMenuList(context);
     return Container(
       padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
       width: MediaQuery.of(context).size.width,
@@ -40,52 +29,60 @@ class _MenuHorizontalState extends State<MenuHorizontal> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: List.generate(
           menuItems.length,
-          (index) => buildMenuItem(index),
+          (index) => buildMenuItem(index, ref),
         ),
       ),
     );
   }
+}
 
-  Widget buildMenuItem(int index) => InkWell(
-        onTap: () {
-          setState(() {
-            selectedIndex = index;
-          });
-        },
-        onHover: (value) {
-          setState(() {
-            value ? hoverIndex = index : hoverIndex = selectedIndex;
-          });
-        },
-        child: Container(
-          height: 100,
-          child: Stack(
-            alignment: Alignment.center,
-            fit: StackFit.loose,
-            children: [
-              Text(
-                menuItems[index],
-                style: TextStyle(fontSize: 20, color: kTextColor),
-              ),
-              // Hover
-              AnimatedPositioned(
-                duration: Duration(milliseconds: 200),
-                left: 0,
-                right: 0,
-                bottom:
-                    selectedIndex != index && hoverIndex == index ? -5 : -32,
-                child: Image.asset("assets/images/Hover.png"),
-              ),
-              // Select
-              AnimatedPositioned(
-                duration: Duration(milliseconds: 200),
-                left: 0,
-                right: 0,
-                bottom: selectedIndex == index ? -1 : -32,
-                child: Image.asset("assets/images/Hover.png"),
-              ),
-            ],
+Widget buildMenuItem(int index, WidgetRef ref) {
+  final menuItems = getMenuList(ref.context);
+  int selectedIndex = ref.watch(selectedIndexProvider);
+  int hoverIndex = ref.watch(hoverIndexProvider);
+
+  MenuHorizontalController menuController = MenuHorizontalController(ref: ref);
+  return InkWell(
+    onTap: () {
+      menuController.onSelect(index);
+    },
+    onHover: (value) {
+      menuController.onHover(value, index);
+    },
+    child: Container(
+      height: 100,
+      child: Stack(
+        alignment: Alignment.center,
+        fit: StackFit.loose,
+        children: [
+          Text(
+            menuItems[index],
+            style: TextStyle(fontSize: 20, color: kTextColor),
           ),
-        ),
-      );
+          // Hover
+          AnimatedPositioned(
+            duration: Duration(milliseconds: 200),
+            left: 0,
+            right: 0,
+            bottom: selectedIndex != index && hoverIndex == index ? -3 : -32,
+            child: Container(
+              height: 6,
+              color: Color(0xFF596a94),
+            ),
+          ),
+          // Select
+          AnimatedPositioned(
+            duration: Duration(milliseconds: 200),
+            left: 0,
+            right: 0,
+            bottom: selectedIndex == index ? -3 : -32,
+            child: Container(
+              height: 6,
+              color: Color(0xFF596a94),
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
